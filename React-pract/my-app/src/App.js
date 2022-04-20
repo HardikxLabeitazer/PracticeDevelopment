8af28b611fb0a65377c0d4b5d323fd10290f44d5
HardikxLabeitazer <hardik.time.leap.2.0.3.6.deceive@gmail.com>
2022-04-20 16:17:52 +0530

    updated

--- React-pract/my-app/src/App.js
+++ React-pract/my-app/src/App.js
@@ -1,19 +1,31 @@
 // import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 import Navbar from './components/Navbar';
-// import TextForm from './components/TextForm';
-import About from './components/About'
+import TextForm from './components/TextForm';
+// import About from './components/About'
 
 function App() {
+  const [mode,setMode]= useState('light');
+  const toggleMode=()=>{
+    if(mode==='light'){
+      setMode('dark');
+      document.body.style.backgroundColor ='grey'
+    }
+    else{
+      setMode('light');
+      document.body.style.backgroundColor ='white'
+    }
+  }
   return (
    <>
-    <Navbar title="TextUtils" aboutText ="About"></Navbar>
-    {/* <div className="container my-3">
-      <TextForm heading="Enter the Text to analyze"/>
-    </div> */}
-    <div className='container'>
-      <About/>
+    <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode}></Navbar>
+    <div className="container my-3">
+      <TextForm heading="Enter the Text to analyze" mode={mode}/>
     </div>
+    {/* <div className='container'>
+      <About/>
+    </div> */}
     
    </>
   );
--- React-pract/my-app/src/components/TextForm.js
+++ React-pract/my-app/src/components/TextForm.js
@@ -5,7 +5,7 @@ import { useState } from 'react'
 
 export default function TextForm(props) {
 
-    const [text, setText] = useState("Enter text here");
+    const [text, setText] = useState("");
     
     const handleUpClick=()=>{
         let newtext = text.toUpperCase();
@@ -44,13 +44,27 @@ export default function TextForm(props) {
         });
         setText(uppercasewords)
     }
+    const handleCopy=()=>{
+        let text = document.getElementById("myBox");
+        text.select();
+        text.setSelectionRange(0,9999);
+        navigator.clipboard.writeText(text.value);
+    }
+    const handleExtraspaces=()=>{
+        let newText = text.split(/[ ]+/);
+        setText(newText.join(" "));
+    }
+    const handleClearText=()=>{
+        let newText = "";
+        setText(newText);
+    }
     return (
         <>
-          <div className='container'>
+          <div className='container' style={{color:props.mode === 'dark'?'white':'black'}}>
             <h1>{props.heading}</h1>
             <div className="mb-3">
                 <label htmlFor="myBox" className="form-label"></label>
-                <textarea className="form-control" onChange={handleOnChange} value={text} id="myBox" rows="8"></textarea>
+                <textarea className="form-control" onChange={handleOnChange} value={text} style={{backgroundColor:props.mode === 'dark'?'grey':'white',color:props.mode === 'dark'?'white':'black'}} id="myBox" rows="8"></textarea>
             </div>
             <button className="btn btn-primary mx-2 my-1" onClick={handleUpClick}>Convert to Uppercase</button>
             <button className="btn btn-primary mx-2 my-1" onClick={handleDownClick}>Convert to LowerCase</button>
@@ -58,8 +72,11 @@ export default function TextForm(props) {
             <button className="btn btn-primary mx-2 my-1" onClick={handleNumExtract}>Extract Numbers</button>
             <button className="btn btn-primary mx-2 my-1" onClick={handleDeleteNumbers}>Remove numbers</button>
             <button className="btn btn-primary mx-2 my-1" onClick={handlefirstcapital}>First Letter Capital</button>
+            <button className="btn btn-primary mx-2 my-1" onClick={handleCopy}>Copy Text</button>
+            <button className="btn btn-primary mx-2 my-1" onClick={handleExtraspaces}>Extra White Spaces</button>
+            <button className="btn btn-primary mx-2 my-1" onClick={handleClearText}>Clear Text</button>
         </div>
-        <div className='container my-3 mx-2' >
+        <div className='container my-3 mx-2' style={{color:props.mode === 'dark'?'white':'dark'}} >
             <h1>Your Text Summary</h1>
             <p>{text.split(" ").length} words and {text.length} characters</p>
             <p>{0.008 * text.split(" ").length} Minutes read</p>
