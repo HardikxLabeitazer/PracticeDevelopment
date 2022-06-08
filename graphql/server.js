59297b5947f756cb96e6bb3ee3a1d5fd29452564
HardikxLabeitazer <hardik.time.leap.2.0.3.6.deceive@gmail.com>
2022-06-09 00:05:23 +0530

    updated

--- graphql/server.js
+++ graphql/server.js
@@ -2,9 +2,22 @@ import {ApolloServer,gql} from 'apollo-server'
 import {ApolloServerPluginLandingPageGraphQLPlayground} from 'apollo-server-core'
 import {quotes,users} from './fakedb.js'
 import typeDefs from './schemagql.js'
+
+import mongoose from 'mongoose'
+
+mongoose.connect("mongodb://localhost:27017/graphqlfirst",()=>{
+    console.log("Connected to mongodb")
+})
+
+mongoose.connection.on("connected",()=>{
+    console.log("Success mongo")
+})
+mongoose.connection.on("error",()=>console.log("Error Mongo"))
+//resolvers
 import resolvers from './resolvers.js'
-
-
+//models
+import './models/Quotes.js'
+import './models/User.js'
 
 const server = new ApolloServer({
     typeDefs,
