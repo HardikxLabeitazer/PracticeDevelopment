3391a4c87f98ed811414e3a89e0593fe3d678049
Hardik Verma <96873709+HardikxLabeitazer@users.noreply.github.com>
2022-06-17 09:43:07 +0530

    Update user.routes.js

--- Skeleton/server/routes/user.routes.js
+++ Skeleton/server/routes/user.routes.js
@@ -33,6 +33,12 @@ router.route('/api/users')
   .get(userCtrl.list)
   .post(userCtrl.create)
 
+router.route('/api/users/name')
+         .get(userCtrl.listname)
+             
+
+
+
 router.route('/api/users/:userId')
   .get(authCtrl.requireSignin, userCtrl.read)
   .put(authCtrl.requireSignin, authCtrl.hasAuthorization, userCtrl.update)
